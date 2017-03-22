a97faa07ed626ad9f4eb32ab5861db468474de36
Ben Ashley (Laptop) <ben@sol3.me>
2017-03-22 13:59:13 +0000

    Auto Mapping and CustomerDTO
    Added auto mapper and a DTO for the customers API.

---
+++ Vidly/DTOs/CustomerDTO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Vidly.Models;
+
+namespace Vidly.DTOs
+{
+    public class CustomerDTO
+    {
+        public int Id { get; set; }
+
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        [Min18YearsIfAMember]
+        public DateTime? Birthdate { get; set; }
+
+        public bool isSubscribedToNewsletter { get; set; }
+
+        [Required]
+        public byte MembershipTypeId { get; set; }
+    }
+}
